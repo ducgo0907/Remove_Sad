a7059474df74bcd5b8d9133112f901c3e3609657
TuNA <nguyentu2392@gmail.com>
2023-10-06 10:52:43 +0700

    fix FE login and register

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -46,39 +46,88 @@ function Login() {
 			})
 	};
 
+	// return (
+	// 	<div className="login-container">
+	// 		<h2>Login</h2>
+	// <form className="login-form" onSubmit={handleSubmit}>
+	// 	<div>
+	// 		<label htmlFor="username">Email:</label>
+	// 		<input
+	// 			type="text"
+	// 			id="username"
+	// 			name="username"
+	// 			value={username}
+	// 			onChange={handleUsernameChange}
+	// 			required
+	// 		/>
+	// 	</div>
+	// 	<div>
+	// 		<label htmlFor="password">Password:</label>
+	// 		<input
+	// 			type="password"
+	// 			id="password"
+	// 			name="password"
+	// 			value={password}
+	// 			onChange={handlePasswordChange}
+	// 			required
+	// 		/>
+	// 	</div>
+	// 	<div>
+	// 		<button type="submit">Login</button><br/>
+	// 		<Link to='/register'>Don't have account ? Go to register</Link>
+	// 	</div>
+	// </form>
+	// 	</div>
+	// );
+
 	return (
-		<div className="login-container">
-			<h2>Login</h2>
-			<form className="login-form" onSubmit={handleSubmit}>
-				<div>
-					<label htmlFor="username">Email:</label>
-					<input
-						type="text"
-						id="username"
-						name="username"
-						value={username}
-						onChange={handleUsernameChange}
-						required
-					/>
+		<div className="p-10">
+			<div className="login-form">
+				<div className="login-wrap">
+					<div className="container p-0">
+						<div className="signIn">
+							Sign In
+						</div>
+						<div className="login-form">
+							<form className="login-form" onSubmit={handleSubmit}>
+								<div className="wrap-input">
+									<label htmlFor="username"></label>
+									<input
+									className="login-input"
+										type="text"
+										id="username"
+										name="username"
+										value={username}
+										onChange={handleUsernameChange}
+										placeholder="Email"
+										required
+									/>
+								</div>
+								<div className="wrap-input">
+									<label htmlFor="password"></label>
+									<input
+										className="login-input"
+										type="password"
+										id="password"
+										name="password"
+										value={password}
+										onChange={handlePasswordChange}
+										placeholder="Password"
+										required
+									/>
+								</div>
+								<div className="mt-3">
+									<button className="login-btn" type="submit">Login</button><br />
+									<Link className="link-register" to='/register'>Don't have account ? Go to register</Link>
+								</div>
+							</form>
+						</div>
+					</div>
 				</div>
-				<div>
-					<label htmlFor="password">Password:</label>
-					<input
-						type="password"
-						id="password"
-						name="password"
-						value={password}
-						onChange={handlePasswordChange}
-						required
-					/>
-				</div>
-				<div>
-					<button type="submit">Login</button><br/>
-					<Link to='/register'>Don't have account ? Go to register</Link>
-				</div>
-			</form>
+			</div>
 		</div>
-	);
+	)
+
 }
 
 export default Login;
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -77,83 +77,180 @@ function Register() {
 	};
 
 	// Inside the RegistrationForm component
+	// return (
+	// 	<div className="registration-container">
+	// 		<h2>Registration</h2>
+	// 		<h3 style={{ color: 'red' }}>{notification}</h3>
+	// <form className="registration-form" onSubmit={handleSubmit}>
+	// 	{/* Name */}
+	// 	<div>
+	// 		<label htmlFor="name">Name:</label>
+	// 		<input
+	// 			type="text"
+	// 			id="name"
+	// 			name="name"
+	// 			value={name}
+	// 			onChange={handleNameChange}
+	// 			required
+	// 		/>
+	// 	</div>
+
+	// 	{/* Email */}
+	// 	<div>
+	// 		<label htmlFor="email">Email:</label>
+	// 		<input
+	// 			type="email"
+	// 			id="email"
+	// 			name="email"
+	// 			value={email}
+	// 			onChange={handleEmailChange}
+	// 			required
+	// 		/>
+	// 	</div>
+
+	// 	{/* Password */}
+	// 	<div>
+	// 		<label htmlFor="password">Password:</label>
+	// 		<input
+	// 			type="password"
+	// 			id="password"
+	// 			name="password"
+	// 			value={password}
+	// 			onChange={handlePasswordChange}
+	// 			required
+	// 		/>
+	// 	</div>
+
+	// 	{/* Phone Number */}
+	// 	<div>
+	// 		<label htmlFor="phoneNumber">Phone Number:</label>
+	// 		<input
+	// 			type="tel"
+	// 			id="phoneNumber"
+	// 			name="phoneNumber"
+	// 			value={phoneNumber}
+	// 			onChange={handlePhoneNumberChange}
+	// 			required
+	// 		/>
+	// 	</div>
+
+	// 	{/* Address */}
+	// 	<div>
+	// 		<label htmlFor="address">Address:</label>
+	// 		<textarea
+	// 			id="address"
+	// 			name="address"
+	// 			value={address}
+	// 			onChange={handleAddressChange}
+	// 			required
+	// 		/>
+	// 	</div>
+
+	// 	{/* Submit Button */}
+	// 	<div>
+	// 		<button type="submit">Register</button><br/>
+	// 		<Link to='/login'>Already have account? Back to login</Link>
+	// 	</div>
+	// </form>
+	// 	</div>
+	// );
+
 	return (
-		<div className="registration-container">
-			<h2>Registration</h2>
-			<h3 style={{ color: 'red' }}>{notification}</h3>
-			<form className="registration-form" onSubmit={handleSubmit}>
-				{/* Name */}
-				<div>
-					<label htmlFor="name">Name:</label>
-					<input
-						type="text"
-						id="name"
-						name="name"
-						value={name}
-						onChange={handleNameChange}
-						required
-					/>
-				</div>
+		<div className='p-10'>
+			<div className='register-form'>
+				<div className='register-wrap'>
+					<div className='container p-0'>
+						<div className='register'>
+							Register
+						</div>
+						<div className='register-form'>
+							<form className="register-form" onSubmit={handleSubmit}>
+								{/* Name */}
+								<div className='wrap-input'>
+									<label htmlFor="name"></label>
+									<input
+										className='register-input'
+										type="text"
+										id="name"
+										name="name"
+										value={name}
+										onChange={handleNameChange}
+										required
+										placeholder='Name'
+									/>
+								</div>
 
-				{/* Email */}
-				<div>
-					<label htmlFor="email">Email:</label>
-					<input
-						type="email"
-						id="email"
-						name="email"
-						value={email}
-						onChange={handleEmailChange}
-						required
-					/>
-				</div>
+								{/* Email */}
+								<div className='wrap-input'>
+									<label htmlFor="email"></label>
+									<input
+										className='register-input'
+										type="email"
+										id="email"
+										name="email"
+										value={email}
+										onChange={handleEmailChange}
+										required
+										placeholder='Email'
+									/>
+								</div>
 
-				{/* Password */}
-				<div>
-					<label htmlFor="password">Password:</label>
-					<input
-						type="password"
-						id="password"
-						name="password"
-						value={password}
-						onChange={handlePasswordChange}
-						required
-					/>
-				</div>
+								{/* Password */}
+								<div className='wrap-input'>
+									<label htmlFor="password"></label>
+									<input
+										className='register-input'
+										type="password"
+										id="password"
+										name="password"
+										value={password}
+										onChange={handlePasswordChange}
+										required
+										placeholder='Password'
+									/>
+								</div>
 
-				{/* Phone Number */}
-				<div>
-					<label htmlFor="phoneNumber">Phone Number:</label>
-					<input
-						type="tel"
-						id="phoneNumber"
-						name="phoneNumber"
-						value={phoneNumber}
-						onChange={handlePhoneNumberChange}
-						required
-					/>
-				</div>
+								{/* Phone Number */}
+								<div className='wrap-input'>
+									<label htmlFor="phoneNumber"></label>
+									<input
+										className='register-input'
+										type="tel"
+										id="phoneNumber"
+										name="phoneNumber"
+										value={phoneNumber}
+										onChange={handlePhoneNumberChange}
+										required
+										placeholder='Phone Number'
+									/>
+								</div>
 
-				{/* Address */}
-				<div>
-					<label htmlFor="address">Address:</label>
-					<textarea
-						id="address"
-						name="address"
-						value={address}
-						onChange={handleAddressChange}
-						required
-					/>
-				</div>
+								{/* Address */}
+								<div className='wrap-input mt-4'>
+									<label htmlFor="address"></label>
+									<textarea
+										className='register-input'
+										id="address"
+										name="address"
+										value={address}
+										onChange={handleAddressChange}
+										required
+										placeholder='Address'
+									/>
+								</div>
 
-				{/* Submit Button */}
-				<div>
-					<button type="submit">Register</button><br/>
-					<Link to='/login'>Already have account? Back to login</Link>
+								{/* Submit Button */}
+								<div className='mt-3'>
+									<button className='register-btn' type="submit">Register</button><br />
+									<Link className='link-login' to='/login'>Already have account? Back to login</Link>
+								</div>
+							</form>
+						</div>
+					</div>
 				</div>
-			</form>
+			</div>
 		</div>
-	);
+	)
 }
 
 export default Register;
