3d3b5c5205edc7871a480e43fc82916ed1a0366a
Trung <ngoquangtrung2047@gmail.com>
2023-10-21 16:14:42 +0700

    Devide admin chat and user chat

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -7,6 +7,7 @@ import CONSTANT from "../../utils/Iconstant";
 import messageService from "../../services/message.service";
 import { v4 as uuidv4 } from "uuid";
 import TextNoti from "../TextNofitication/TextNoti";
+import AdminChat from "../AdminChat/AdminChat";
 
 // const host = "https://s9fyy9-3001.csb.app";
 const host = CONSTANT.host;
@@ -130,81 +131,20 @@ function Chat({ userLogged, setSocket, socket }) {
 		}
 	}, [socket])
 	return (
-		<div className="flex justify-center w-full">
-			{!userLogged.isAdmin && !isConnect && <TextNoti text={"watiting ...."} />}
-			<div>
-				<h2>Hello {userName} {isAdmin && ', these are your list customer: '}</h2>
-				<ul className="user-list"> {/* Apply the user-list class */}
-					{users.map((userDetail) => userDetail.sender !== userLogged.email && isAdmin ? (
-						<li key={userDetail.sender} onClick={() => setUser(userDetail.sender)} className={userDetail.sender === user ? "isSelected user-item" : "user-item"}> {/* Apply the user-item class */}
-							<span className="user-name">{userDetail.sender}</span> {/* Apply the user-name class */}
-						</li>
-					) : (<></>))}
-				</ul>
-			</div>
-			<section className="msger w-full">
-				<header className="msger-header">
-					<div className="msger-header-title">
-						<i className="fas fa-comment-alt"></i> Pilyr Chat
-					</div>
-					<div className="msger-header-options">
-						<span><i className="fas fa-cog"></i></span>
-					</div>
-				</header>
-
-				<main className="msger-chat">
-					<ScrollToBottom className="message-container">
-						{mess.map(message => {
-							return message.isAdmin ?
-								(
-									<div className="msg left-msg" key={uuidv4()}>
-										<div
-											className="msg-img"
-											style={{ backgroundImage: "url(https://ih1.redbubble.net/image.2610089591.4691/pp,504x498-pad,600x600,f8f8f8.jpg)" }}
-										></div>
-
-										<div className="msg-bubble">
-											<div className="msg-info">
-												<div className="msg-info-name">Pilyr</div>
-												<div className="msg-info-time">{message.createdAt} {message.isAdmin}</div>
-											</div>
-
-											<div className="msg-text text-left">
-												{message.message}
-											</div>
-										</div>
-									</div>
-								) :
-								(
-									<div className="msg right-msg" key={uuidv4()}>
-										<div
-											className="msg-img"
-										></div>
-
-										<div className="msg-bubble">
-											<div className="msg-info">
-												<div className="msg-info-name">{message.fakeName}</div>
-												<div className="msg-info-time">{message.createdAt}</div>
-											</div>
-
-											<div className="msg-text whitespace-pre-wrap">
-												{message.message}
-											</div>
-										</div>
-									</div>
-								)
-						})}
-					</ScrollToBottom>
-				</main>
-				<div className="send-msg">
-					<input type="text" className="msger-input"
-						placeholder="Enter your message..."
-						value={message}
-						onKeyDown={onEnterPerss}
-						onChange={e => setMessage(e.target.value)} />
-					<button type="submit" className="msger-send-btn m-0" onClick={sendMessage} >Send</button>
-				</div>
-			</section>
+		<div>
+			<AdminChat
+				userLogged={userLogged}
+				isConnect={isConnect}
+				userName={userName}
+				isAdmin={isAdmin}
+				users={users}
+				user={user}
+				mess={mess}
+				setUser={setUser}
+				message={message}
+				onEnterPerss={onEnterPerss}
+				setMessage={setMessage}
+				sendMessage={sendMessage} />
 		</div>
 	);
 }
