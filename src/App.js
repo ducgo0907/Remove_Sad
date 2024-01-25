b3eaf0e39bda1d3b8e76f09b0b4b3177bafedd54
TuNA <nguyentu2392@gmail.com>
2024-01-25 22:06:20 +0700

    fix logo

--- src/App.js
+++ src/App.js
@@ -106,8 +106,8 @@ function App() {
 			<div className="App">
 				<header className="relative">
 					<nav className="navbar navbar-expand-lg navbar-light bg-light fixed top-0 left-0 right-0">
-						<div className="container">
-							<Link className="navbar-brand" to="/">Pilyr</Link>
+						<div className="container ">
+							<Link className="navbar-brand" to="/"><img className='w-24' src='pilyr-logo.png'/></Link>
 							<div className='flex flex-row space-x-10'>
 								<Link className="navbar-brand font-thin" to="/home">Home</Link>
 								<Link className="navbar-brand font-thin" to="/about">About</Link>
@@ -115,10 +115,10 @@ function App() {
 								<div className='navbar-brand font-thin'>
 									Bạn đang có {money ? money / 20000 : 0} cốc coffee
 								</div>
-								<Link to='/payment' className='font-semibold text-2xl	' style={{ textDecoration: "none", fontSize: "" }}>Nạp tiền</Link>
+								<Link to='/payment' className='font-semibold text-xl	' style={{ textDecoration: "none", fontSize: "" }}>Nạp tiền</Link>
 								{user && user.email != undefined && user.email.includes("@")
-									? (<div className='narbar-brand logout-btn' onClick={() => logOut()}>Đăng xuất</div>)
-									: (<Link className='navbar-brand' to="/login">Đăng nhập</Link>)}
+									? (<div className='narbar-brand logout-btn font-thin text-xl' onClick={() => logOut()}>Đăng xuất</div>)
+									: (<Link className='navbar-brand font-thin' to="/login">Đăng nhập</Link>)}
 								{/* <Link className="navbar-brand font-thin" to="/">Blog</Link>
 								<Link className="navbar-brand font-thin" to="/">Contact</Link> */}
 							</div>
